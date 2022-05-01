0290e6a82c8f78963279eee2c900cf77b21bcfb6
rodonisi <simon@rodoni.ch>
2022-05-02 00:02:46 +0200

    [models] add app state model tests

---
+++ test/models/app_state_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:reports/models/app_state.dart';
+
+void main() {
+  late AppStateModel target;
+  setUpAll(() {
+    target = AppStateModel();
+  });
+  test('enitialization', () {
+    var expectedPage = Pages.reports;
+    var expectedPath = '';
+
+    expect(target.currentPage, expectedPage);
+    expect(target.reportsListPath, expectedPath);
+  });
+
+  test('set currentPage', () {
+    var expectedPage = Pages.layouts;
+    var appState = AppStateModel();
+
+    appState.currentPage = expectedPage;
+
+    expect(appState.currentPage, expectedPage);
+  });
+
+  test('set reportsListPath', () {
+    var expectedPath = 'test';
+
+    target.reportsListPath = expectedPath;
+
+    expect(target.reportsListPath, expectedPath);
+  });
+}
