739313c7db1d2657417d3af6d0c8359e1dcdaa01
rodonisi <simon@rodoni.ch>
2022-05-08 20:11:06 +0200

    [test] Add WrapNavigator test

---
+++ test/widgets/wrap_navigator_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:reports/widgets/wrap_navigator.dart';
+
+import '../utilities.dart';
+
+void main() {
+  late Widget target;
+  final key = ValueKey('container');
+  final key2 = ValueKey('container2');
+  setUpAll(() {});
+
+  testWidgets('one child', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: WrapNavigator(
+          child: MaterialPage(
+            child: Container(
+              key: key,
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(find.byKey(key), findsOneWidget);
+  });
+
+  testWidgets('multiple pages', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: WrapNavigator(
+          child: MaterialPage(
+            child: Container(
+              key: key,
+            ),
+          ),
+          additionalPages: [
+            MaterialPage(
+              child: Scaffold(
+                appBar: AppBar(),
+                body: Container(
+                  key: key2,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    // The second container should be visible.
+    expect(find.byKey(key2), findsOneWidget);
+    expect(find.byKey(key), findsNothing);
+
+    // Pop the page.
+    await tester.pageBack();
+    await tester.pump();
+
+    // The first container should be visible.
+    expect(find.byKey(key), findsOneWidget);
+  });
+}
